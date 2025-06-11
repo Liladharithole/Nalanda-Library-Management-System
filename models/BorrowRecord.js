e076632acf143cdf96b158d6414fbad2a69f8a54
LILADHAR <97004436+Liladharithole@users.noreply.github.com>
2025-06-11 18:26:38 +0530

    Create Models

--- models/BorrowRecord.js
+++ models/BorrowRecord.js
@@ -23,4 +23,4 @@ const borrowSchema = new mongoose.Schema(
   { timestamps: true }
 );
 
-module.exports = mongoose.model("BorrowRecord", borrowSchema);
+export default mongoose.model("BorrowRecord", borrowSchema);
